54e088633904fd347aa5e846c16c3427315c29b7
jjgoings <jjgoings@gmail.com>
2021-07-27 17:37:22 -0700

    generate rank-reduced integrals if they do not exist for SF and DF

--- src/chemftr/df/compute_lambda_df.py
+++ src/chemftr/df/compute_lambda_df.py
@@ -30,28 +30,47 @@ def compute_lambda(thresh: float, integral_name: str, verify_eri: bool = False)\
     with h5py.File(os.path.join(cwd, "..", "integrals",
                                 "eri_"+integral_name+".h5"), "r") as f:
         eri = np.asarray(f['eri'][()])
-        h0 = np.asarray(f['h0'][()])
-
-    with h5py.File(os.path.join(cwd, "..", "integrals",
-                                "eri_"+integral_name+"_cholesky.h5"), "r") as f:
-        cholesky_diagonals = np.asarray(f["gval"][()])
-        cholesky_lower_tri = np.asarray(f["gvec"][()])
+        h0  = np.asarray(f['h0'][()])
 
     n_orb = len(h0)  # number orbitals
-
     # Check dims are consistent
     assert [n_orb] * 4 == [*eri.shape]
 
-    nchol_max = cholesky_diagonals.shape[0]
+    try:
+        with h5py.File(os.path.join(cwd, "..", "integrals",
+                                    "eri_"+integral_name+"_cholesky.h5"), "r") as f:
+            cholesky_diagonals = np.asarray(f["gval"][()])
+            cholesky_lower_tri = np.asarray(f["gvec"][()])
+
+    except FileNotFoundError:
+
+        # Cholesky factored ints do not exist, so create them
+        cholesky_diagonals, cholesky_lower_tri = np.linalg.eigh(eri.reshape(n_orb **2, n_orb **2))
+
+        # Put in descending order
+        cholesky_diagonals = cholesky_diagonals[::-1]
+        cholesky_lower_tri = cholesky_lower_tri[:,::-1]
+
+        # Truncate
+        # FIXME: add user-defined threshold (usually 1E-8 is more than enough)
+        idx = np.where(cholesky_diagonals > 1.15E-16)[0]
+        cholesky_diagonals, cholesky_lower_tri = cholesky_diagonals[idx], cholesky_lower_tri[:,idx]
+
+        #FIXME: Add option to save rank reduced integrals
 
     # eliminate diagonals D from Cholesky decomposition LDL^T
-    L = np.einsum("ij,j->ji",cholesky_lower_tri,
-        np.sqrt(cholesky_diagonals)).reshape(nchol_max, n_orb, n_orb)
+    L = np.einsum("ij,j->ij",cholesky_lower_tri,
+        np.sqrt(cholesky_diagonals))
 
     if verify_eri:
         # Make sure we are reading in the integrals correctly ... don't check for large cases (!)
-        eri_new = np.einsum("ijP,Pkl->ijkl",L.T,L,optimize=True)
+        eri_new = np.einsum('ik,kj->ij',L,L.T,optimize=True)
         assert np.allclose(eri_new.flatten(),eri.flatten())
+
+    # Reshape for lambda calcs
+    L = L.reshape(n_orb, n_orb, -1)
+
+    nchol_max = cholesky_diagonals.shape[0]
 
     T = h0 - 0.5 * np.einsum("illj->ij", eri) + np.einsum("llij->ij", eri)
     e, v = np.linalg.eigh(T)
@@ -64,7 +83,7 @@ def compute_lambda(thresh: float, integral_name: str, verify_eri: bool = False)\
 
     M = 0 # rolling number of eigenvectors
     for R in range(nchol_max):
-        Lij = L[R, :,:]
+        Lij = L[:,:, R]
         e, v = np.linalg.eigh(Lij)
         normSC = np.sum(np.abs(e))
 
--- src/chemftr/sf/compute_lambda_sf.py
+++ src/chemftr/sf/compute_lambda_sf.py
@@ -30,24 +30,43 @@ def compute_lambda(cholesky_dim: int, integral_name: str, verify_eri: bool = Fal
         eri = np.asarray(f['eri'][()])
         h0  = np.asarray(f['h0'][()])
 
-    with h5py.File(os.path.join(cwd, "..", "integrals",
-                                "eri_"+integral_name+"_cholesky.h5"), "r") as f:
-        cholesky_diagonals = np.asarray(f["gval"][()])
-        cholesky_lower_tri = np.asarray(f["gvec"][()])
-
     n_orb = len(h0)  # number orbitals
-
     # Check dims are consistent
     assert [n_orb] * 4 == [*eri.shape]
 
+    try:
+        with h5py.File(os.path.join(cwd, "..", "integrals",
+                                    "eri_"+integral_name+"_cholesky.h5"), "r") as f:
+            cholesky_diagonals = np.asarray(f["gval"][()])
+            cholesky_lower_tri = np.asarray(f["gvec"][()])
+
+    except FileNotFoundError:
+
+        # Cholesky factored ints do not exist, so create them
+        cholesky_diagonals, cholesky_lower_tri = np.linalg.eigh(eri.reshape(n_orb **2, n_orb **2))
+
+        # Put in descending order
+        cholesky_diagonals = cholesky_diagonals[::-1]
+        cholesky_lower_tri = cholesky_lower_tri[:,::-1]
+
+        # Truncate
+        # FIXME: add user-defined threshold (usually 1E-8 is more than enough)
+        idx = np.where(cholesky_diagonals > 1.15E-16)[0]
+        cholesky_diagonals, cholesky_lower_tri = cholesky_diagonals[idx], cholesky_lower_tri[:,idx]
+
+        #FIXME: Add option to save rank reduced integrals
+
     # eliminate diagonals D from Cholesky decomposition LDL^T
-    L = np.einsum("ij,j->ji",cholesky_lower_tri,
-        np.sqrt(cholesky_diagonals)).reshape(-1, n_orb, n_orb)
+    L = np.einsum("ij,j->ij",cholesky_lower_tri,
+        np.sqrt(cholesky_diagonals))
 
     if verify_eri:
         # Make sure we are reading in the integrals correctly ... don't check for large cases (!)
-        eri_new = np.einsum("ijP,Pkl->ijkl",L.T,L,optimize=True)
+        eri_new = np.einsum('ik,kj->ij',L,L.T,optimize=True)
         assert np.allclose(eri_new.flatten(),eri.flatten())
+
+    # Reshape for lambda calcs
+    L = L.reshape(n_orb, n_orb, -1)
 
     # Effective one electron operator contribution
     T = h0 - 0.5 * np.einsum("pqqs->ps", eri, optimize=True) +\
@@ -56,10 +75,10 @@ def compute_lambda(cholesky_dim: int, integral_name: str, verify_eri: bool = Fal
     lambda_T = np.sum(np.abs(T))
 
     # Do rank-reduction of ERIs using cholesky_dim vectors
-    LR = L[:cholesky_dim,:,:]
+    LR = L[:,:,:cholesky_dim]
 
     # Two electron operator contributions
-    lambda_W = 0.25 * np.einsum("Pij,Pkl->",np.abs(LR), np.abs(LR), optimize=True)
+    lambda_W = 0.25 * np.einsum("ijP,klP->",np.abs(LR), np.abs(LR), optimize=True)
     lambda_tot = lambda_T + lambda_W
 
     return n_orb * 2, lambda_tot  #  return number spin orbitals from spatial and combined lambda
