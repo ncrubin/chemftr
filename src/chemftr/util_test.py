5cbd76d2cd477294af781d0201fe14f8ccbcc301
jjgoings <jjgoings@gmail.com>
2021-08-03 08:52:57 -0700

    add full CCSD_T test

--- src/chemftr/util_test.py
+++ src/chemftr/util_test.py
@@ -43,3 +43,54 @@ def test_power_two():
     assert power_two(104) == 3  # 2**3 * 13
     assert power_two(128) == 7  # 2**7
     assert power_two(393120) == 5  # 2**5 * 3**3 * 5 * 7 * 13
+
+def full_ccsd_t():
+    """ Test chemftr full CCSD(T) from h1/eri/ecore tensors matches regular PySCF CCSD(T) """
+
+    for scf_type in ['rhf', 'rohf']:
+        mol = gto.Mole()
+        mol.atom = 'H 0 0 0; F 0 0 1.1'
+        mol.charge = 0
+        if scf_type == 'rhf':
+            mol.spin = 0
+        elif scf_type == 'rohf':
+            mol.spin = 2
+        mol.basis = 'ccpvtz'
+        mol.symmetry = False
+        mol.build()
+
+        if scf_type == 'rhf':
+            mf = scf.RHF(mol)
+        elif scf_type == 'rohf':
+            mf = scf.ROHF(mol) 
+
+        mf.init_guess = 'mindo'
+        mf.conv_tol = 1e-10
+        mf.kernel()
+        mo1 = mf.stability()[0]
+        dm1 = mf.make_rdm1(mo1, mf.mo_occ)
+        mf = mf.run(dm1)
+
+        # Do PySCF CCSD(T)
+        mycc = cc.CCSD(mf)
+        mycc.max_cycle = 500
+        mycc.conv_tol = 1E-7
+        mycc.conv_tol_normt = 1E-5
+        mycc.diis_space = 24
+        mycc.diis_start_cycle = 4
+        mycc.kernel()
+        et = mycc.ccsd_t()
+
+        pyscf_escf = mf.e_tot
+        pyscf_ecor = mycc.e_corr + et
+        pyscf_etot = pyscf_escf + pyscf_ecor
+        pyscf_results = np.arrray([pyscf_escf, pyscf_ecor, pyscf_etot])
+
+        n_elec = mol.nelectron
+        n_orb = mf.mo_coeff[0].shape[-1]
+
+        chemftr_results = ccsd_t(gen_cas(mf, n_orb, n_elec))
+        chemftr_results = np.asarray(chemftr_results)
+
+        # ignore relative tolerance, we just want absolute tolerance
+        assert np.allclose(pyscf_results,chemftr_results,rtol=1E-14)
