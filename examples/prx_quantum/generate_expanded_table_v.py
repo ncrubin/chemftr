a5f94fe01575abe56964d8a0726e9228c8ce23ac
jjgoings <jjgoings@gmail.com>
2021-09-18 10:18:09 -0700

    fix number of electrons for Li example

--- examples/prx_quantum/generate_expanded_table_v.py
+++ examples/prx_quantum/generate_expanded_table_v.py
@@ -18,7 +18,7 @@ BETA = 20
 
 # eri_LI.h5 can be found at https://doi.org/10.5281/zenodo.4248322
 LI_INTS = files('chemftr.integrals').joinpath('eri_li.h5')  # pre-packaged integrals
-li_mol, li_mf = load_casfile_to_pyscf(LI_INTS, num_alpha = 27, num_beta = 27)
+li_mol, li_mf = load_casfile_to_pyscf(LI_INTS, num_alpha = 74, num_beta = 39)
 
 # This writes out to file "thc_factorization_LI_femoco.txt"
 thc.generate_costing_table(li_mf, name='li_femoco', nthc_range=range(250,801,50), dE=DE, chi=CHI, beta=BETA, save_thc=False)
